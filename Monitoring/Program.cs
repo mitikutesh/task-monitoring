cd595e6e2bcdf47358354792f3e3cf95ad15e6fa
Mitiku Teshome <mitikutesh@gmail.com>
2020-12-26 17:08:20 +0200

    -add hosted service

--- Monitoring/Program.cs
+++ Monitoring/Program.cs
@@ -1,12 +1,91 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Monitoring.Services;
+using static Microsoft.Extensions.Hosting.EnvironmentName;
 
 namespace Monitoring
 {
     class Program
     {
-        static void Main(string[] args)
+       
+        static async  Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var isService = !(Debugger.IsAttached || args.Contains("--console"));
+                if (isService)
+                {
+                    var processModule = Process.GetCurrentProcess().MainModule;
+                    if (processModule != null)
+                    {
+                        var pathToExe = processModule.FileName;
+                        var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                        Directory.SetCurrentDirectory(pathToContentRoot);
+                    }
+                    else
+                    {
+                        //TODO
+                    }
+                }
+
+                //using generic host
+                var host = new HostBuilder()
+                    .ConfigureServices((_, services) =>
+                    {
+                        services.AddHostedService<HostedMonitoringServiceBase>();
+                    });
+                
+                host.UseEnvironment(isService ? Production : Development);
+
+                
+                if (isService)
+                {
+                    await host.RunAsServiceAsync();
+                }
+                else
+                {
+                    host.UseServiceBaseLifetime()
+                        .ConfigureHostConfiguration(config =>
+                        {
+                            config.AddCommandLine(args);
+                            if (args != null)
+                            {
+                                config.AddCommandLine(args);
+                            }
+                        });
+                    await host.RunConsoleAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                File.WriteAllText(@"D:\Temp\MitikuServiceBug.txt", ex.ToString());
+                //TODO log error
+            }
+            finally
+            {
+                //clearn up
+            }
+
         }
+
+        // static IHostBuilder CreateHostBuilder(string[] args) =>
+        //     Host.CreateDefaultBuilder(args)
+        //         .ConfigureServices((_, services) =>
+        //             services.AddHostedService<HostedMonitoringService>());
+
+        // .ConfigureHostConfiguration(configHost =>
+        // {
+        //     configHost.SetBasePath(Directory.GetCurrentDirectory());
+        //     configHost.AddJsonFile("hostsettings.json", optional: true);
+        //     configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+        //     configHost.AddCommandLine(args);
+        // });
     }
 }
